c5a2ae5b43acd5323050edb4a3fb8f234c326b86
Simon Gravelle <simon.gravelle@live.fr>
2023-05-25 23:28:23 +0200

    worked on py/bash

--- 2023-gravelle-preprint/back-mapping/initialise-lammps.sh
+++ 2023-gravelle-preprint/back-mapping/initialise-lammps.sh
@@ -2,109 +2,112 @@
 
 set -e
 
+# path to lammps
+lmp=/home/simon/Softwares/lammps-8Feb2023/src/lmp_serial 
+
 # Extract the positions of the bead, and create list of positions
 # Those list will later be used in LAMMPS
-cp python-script/GenerateTether.py .
+cp python-scripts/GenerateTether.py .
 python3 GenerateTether.py
 rm GenerateTether.py
 
 while IFS="" read -r p || [ -n "$p" ]
 do
 
-  cpt_PEG=$((p + 1))
+    cpt_PEG=$((p + 1))
 
-  WORKING_DIR=".tempPEG"$cpt_PEG
+    WORKING_DIR="_PEG"$cpt_PEG
 
-  mkdir -p WORKING_DIR
-  cp PEG.list WORKING_DIR
-  cd WORKING_DIR
-  
-  stop
+    mkdir -p ${WORKING_DIR}
+    #cp _PEG.list ${WORKING_DIR}
+    cd ${WORKING_DIR}
 
-  printf 'Dealing with PEG%s\n' "$cpt_PEG"
+        printf 'Dealing with PEG%s\n' "$cpt_PEG"
 
-  # copy data file, and replace the lines
-  cp ../PEG/PEG.data ../data/PEG.$cpt_PEG.data
+        # copy data file, and replace the lines
+        cp ../PEG-parameters/PEG.data ../_lammps-data/PEG.$cpt_PEG.data
 
-  # copy input file, and replace the lines
-  cp ../inputs/input.prepare.lammps input.PEG$cpt_PEG.lammps
-  newline='read_data ../data/PEG.'$cpt_PEG'.data'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'read_data PEG.data')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='write_data ../data/PEG.'$cpt_PEG'.data'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_data PEG.data')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='write_dump all atom ../dump/PEG'$cpt_PEG'/configuration.lammpstrj'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_dump all atom')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='write_dump all xyz ../dump/PEG'$cpt_PEG'/configuration.xyz'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_dump all xyz')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        # copy input file, and replace the lines
+        cp ../lammps-inputs/input.prepare.lammps input.PEG$cpt_PEG.lammps
 
-  # read tether positions
-  filex=../tether/PEG$cpt_PEG/ns0/TETHER.x.lammps
-  filey=../tether/PEG$cpt_PEG/ns0/TETHER.y.lammps
-  filez=../tether/PEG$cpt_PEG/ns0/TETHER.z.lammps
-  readarray -t arrayx < $filex
-  readarray -t arrayy < $filey
-  readarray -t arrayz < $filez
+        newline='read_data ../_lammps-data/PEG.'$cpt_PEG'.data'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'read_data PEG.data')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
 
-  # extract tether positions 
-  IFS=', ' read -r -a linex <<< "${arrayx[i]}"
-  IFS=', ' read -r -a liney <<< "${arrayy[i]}"
-  IFS=', ' read -r -a linez <<< "${arrayz[i]}"
-  x1=${linex[0]}
-  x2=${linex[1]}
-  x3=${linex[2]}
-  x4=${linex[3]}
-  x5=${linex[4]}
-  y1=${liney[0]}
-  y2=${liney[1]}
-  y3=${liney[2]}
-  y4=${liney[3]}
-  y5=${liney[4]}
-  z1=${linez[0]}
-  z2=${linez[1]}
-  z3=${linez[2]}
-  z4=${linez[3]}
-  z5=${linez[4]}
+        newline='write_data ../_lammps-data/PEG.'$cpt_PEG'.data'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_data PEG.data')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
 
-  # update spring tether positions
-  newline='fix pull1 MON1 spring tether 10000 '$x1' '$y1' '$z1' 0'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull1 MON1 spring tether')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='fix pull2 MON2 spring tether 10000 '$x2' '$y2' '$z2' 0'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull2 MON2 spring tether')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='fix pull3 MON3 spring tether 10000 '$x3' '$y3' '$z3' 0'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull3 MON3 spring tether')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='fix pull4 MON4 spring tether 10000 '$x4' '$y4' '$z4' 0'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull4 MON4 spring tether')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
-  newline='fix pull5 MON5 spring tether 10000 '$x5' '$y5' '$z5' 0'
-  linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull5 MON5 spring tether')
-  sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        newline='write_dump all atom ../_lammps-dump/PEG'$cpt_PEG'/configuration.lammpstrj'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_dump all atom')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
 
-  # run lammps
-  
-  {
-      /home/simon/Softwares/lammps-28Mar2023/src/lmp_serial -in -in input.PEG$cpt_PEG.lammps
-  }  &> /dev/null
+        newline='write_dump all xyz ../_lammps-dump/PEG'$cpt_PEG'/configuration.xyz'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'write_dump all xyz')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
 
-  cp ../AssertLAMMPS.py .
-  python3 AssertLAMMPS.py
+        # read tether positions and extract the 5 tether positions 
+        readarray -t arrayx < ../_tether_positions/PEG$cpt_PEG/TETHER.x.lammps
+        readarray -t arrayy < ../_tether_positions/PEG$cpt_PEG/TETHER.y.lammps
+        readarray -t arrayz < ../_tether_positions/PEG$cpt_PEG/TETHER.z.lammps
 
-  cd ..
+        IFS=', ' read -r -a linex <<< "${arrayx[i]}"
+        x1=${linex[0]}
+        x2=${linex[1]}
+        x3=${linex[2]}
+        x4=${linex[3]}
+        x5=${linex[4]}
 
-  cp 'data/PEG.'$cpt_PEG'.data'  'data/PEG.'$cpt_PEG'_0.data'
+        IFS=', ' read -r -a liney <<< "${arrayy[i]}"
+        y1=${liney[0]}
+        y2=${liney[1]}
+        y3=${liney[2]}
+        y4=${liney[3]}
+        y5=${liney[4]}
 
-  # rm -r "tempPEG"$cpt_PEG
-done < PEG.list
+        IFS=', ' read -r -a linez <<< "${arrayz[i]}"
+        z1=${linez[0]}
+        z2=${linez[1]}
+        z3=${linez[2]}
+        z4=${linez[3]}
+        z5=${linez[4]}
 
-rm GenerateTether.py
+        # update spring tether positions
+        newline='fix pull1 MON1 spring tether 10000 '$x1' '$y1' '$z1' 0'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull1 MON1 spring tether')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        newline='fix pull2 MON2 spring tether 10000 '$x2' '$y2' '$z2' 0'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull2 MON2 spring tether')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        newline='fix pull3 MON3 spring tether 10000 '$x3' '$y3' '$z3' 0'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull3 MON3 spring tether')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        newline='fix pull4 MON4 spring tether 10000 '$x4' '$y4' '$z4' 0'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull4 MON4 spring tether')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
+        newline='fix pull5 MON5 spring tether 10000 '$x5' '$y5' '$z5' 0'
+        linetoreplace=$(cat input.PEG$cpt_PEG.lammps | grep 'fix pull5 MON5 spring tether')
+        sed -i '/'"$linetoreplace"'/c\'"$newline" input.PEG$cpt_PEG.lammps
 
-bash RunLAMMPS.sh &
+        # run lammps
+        {
+            ${lmp} -in input.PEG$cpt_PEG.lammps
+        } &> /dev/null
+
+        # ensure that the LAMMPS simulation ran without error
+        cp ../python-scripts/AssertLAMMPS.py .
+        python3 AssertLAMMPS.py
+        rm AssertLAMMPS.py
+
+    cd ..
+
+    #cp 'data/PEG.'$cpt_PEG'.data'  'data/PEG.'$cpt_PEG'_init.data'
+
+    # rm -r "tempPEG"$cpt_PEG
+done < _PEG.list
+
+
+#bash RunLAMMPS.sh &
 
 # delete temp folder
 
@@ -117,10 +120,10 @@ bash RunLAMMPS.sh &
 #  fi
 #done < PEG.list
 
-cd dump
-  cp ../MergeUniverse.py .
-  python3 MergeUniverse.py
-cd ..
+#cd dump
+#  cp ../MergeUniverse.py .
+#  python3 MergeUniverse.py
+#cd ..
 
 
 
--- 2023-gravelle-preprint/back-mapping/python-scripts/GenerateTether.py
+++ 2023-gravelle-preprint/back-mapping/python-scripts/GenerateTether.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[23]:
-
-
 import MDAnalysis as mda
 import numpy as np
 import os
 import gc
 
-def make_tether(u, PEG, cpt_res):
+folder = "_tether_positions/"
+
+def make_tether(u, PEG, cpt_res, folder):
     allx, ally, allz = [], [], []
     for _ in u.trajectory:
         allx.append(PEG.atoms.positions.T[0])
         ally.append(PEG.atoms.positions.T[1])
         allz.append(PEG.atoms.positions.T[2])
-    f1 = open('tether/PEG'+str(cpt_res+1)+'/TETHER.x.lammps', 'w')
-    f2 = open('tether/PEG'+str(cpt_res+1)+'/TETHER.y.lammps', 'w')
-    f3 = open('tether/PEG'+str(cpt_res+1)+'/TETHER.z.lammps', 'w')
+    f1 = open(folder+'PEG'+str(cpt_res+1)+'/TETHER.x.lammps', 'w')
+    f2 = open(folder+'PEG'+str(cpt_res+1)+'/TETHER.y.lammps', 'w')
+    f3 = open(folder+'PEG'+str(cpt_res+1)+'/TETHER.z.lammps', 'w')
     for x, y, z in zip(allx, ally, allz):
         f1.write(str(np.round(x[0],2))+' '+str(np.round(x[1],2))+' '+str(np.round(x[2],2))+' '+str(np.round(x[3],2))+' '+str(np.round(x[4],2))+'\n')
         f2.write(str(np.round(y[0],2))+' '+str(np.round(y[1],2))+' '+str(np.round(y[2],2))+' '+str(np.round(y[3],2))+' '+str(np.round(y[4],2))+'\n')
@@ -26,10 +25,10 @@ def make_tether(u, PEG, cpt_res):
     f2.close()
     f3.close()
 
-if not os.path.exists('dump/'):
-    os.mkdir('dump')
-if not os.path.exists('data/'):
-    os.mkdir('data')
+if not os.path.exists('_lammps-dump/'):
+    os.mkdir('_lammps-dump')
+if not os.path.exists('_lammps-data/'):
+    os.mkdir('_lammps-data/')
 
 # original CG trajectory
 gro = "C-CG/conf.gro"
@@ -41,28 +40,29 @@ n_frames = original_univers.trajectory.n_frames
 timestep = np.round(original_univers.trajectory.dt,2)
 
 # save and import dimensions, not sure why
-f = open("dump/dimensions.dat", "w")
+f = open("_lammps-dump/dimensions.dat", "w")
 f.write(str(dimensions[0])+' '+str(dimensions[1])+' '+str(dimensions[2])+' '+str(dimensions[3])+' '+str(dimensions[4])+' '+str(dimensions[5])+'\n')
 f.close()
-dimensions = np.loadtxt("dump/dimensions.dat")
+#dimensions = np.loadtxt("dump/dimensions.dat")
 
 # detect the peg list from the CG trajecotry
 PEG_list = []
 for cpt_res, ids in enumerate(original_univers.residues.ids):
     PEG_list.append(cpt_res)
-f = open('PEG.list', 'w')
+f = open('_PEG.list', 'w')
 for x in PEG_list:
     f.write(str(x)+'\n')
 f.close()
 
-if not os.path.exists('tether/'):
-    os.mkdir('tether')
+if not os.path.exists(folder):
+    os.mkdir(folder)
+
 for cpt_PEG in PEG_list:
-    if not os.path.exists('dump/PEG'+str(cpt_res+1)):
-        os.mkdir('dump/PEG'+str(cpt_res+1))
-    if not os.path.exists('tether/PEG'+str(cpt_PEG+1)):
-        os.mkdir('tether/PEG'+str(cpt_PEG+1))
+    if not os.path.exists('_lammps-dump/PEG'+str(cpt_PEG+1)):
+        os.mkdir('_lammps-dump/PEG'+str(cpt_PEG+1))
+    if not os.path.exists(folder+'PEG'+str(cpt_PEG+1)):
+        os.mkdir(folder+'PEG'+str(cpt_PEG+1))
     u = mda.Universe(gro, trj)
-    PEG = u.select_atoms('resindex '+str(cpt_PEG)) # select the cpt_PEG PEG molecule
-    make_tether(u, PEG, cpt_PEG)
+    PEG = u.select_atoms('resindex '+str(cpt_PEG)) # select the PEG molecule with id cpt_PEG
+    make_tether(u, PEG, cpt_PEG, folder)
 
