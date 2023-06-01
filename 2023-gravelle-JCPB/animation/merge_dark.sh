8415fc593fd47539d3d3b0bf5e51b09641635620
Simon Gravelle <simon.gravelle@live.fr>
2023-06-01 11:13:56 +0200

    improved merging procedure

--- 2023-gravelle-JCPB/animation/merge_dark.sh
+++ 2023-gravelle-JCPB/animation/merge_dark.sh
@@ -1,4 +1,4 @@
-	#!/bin/bash
+#!/bin/bash
 # Credit to Otello M Roscioni from Bristol : https://matsci.org/u/hothello
 
 export LC_NUMERIC="en_US.UTF-8"
@@ -7,26 +7,26 @@ set -e
 
 cd AA-white
 	cp ../transparent-white.sh .
-	#bash transparent-white.sh
+	bash transparent-white.sh
 	rm transparent-white.sh
 cd ..
 cd CG-white
 	cp ../transparent-white.sh .
-	#bash transparent-white.sh
+	bash transparent-white.sh
 	rm transparent-white.sh
 cd ..
 
 for i in {0..199}; 
 do
 
-    x1=$(printf "%.0f" $(bc -l<<<\($i+1\)*7))
-    x2=$(printf "%.0f" $(bc -l<<<\($i+1\)*10))
+    x1=$(printf "%.0f" $(bc -l<<<\($i+1\)*2.083333))
+    x2=$(printf "%.0f" $(bc -l<<<\($i+	1\)*5.2083333))
 
     j=$(printf "%05i" $(bc -l<<<$i))
-    
+
     # create a linear gradient file
-    convert -size 1920x1136 -define gradient:vector=$x1,500,$x2,500,angle=90 gradient:white-black _linear_gradient.png
-	# compose the 2 images
+    convert -size 1920x1136 -define gradient:vector=$x1,500,$x2,500,angle=90 gradient:black-white _linear_gradient.png
+
     convert CG-white/_untitled.$j.ppm.png _linear_gradient.png -alpha remove -compose CopyOpacity -composite AA-white/_untitled.$j.ppm.png -compose DstOver -composite _base_out.png
     convert -alpha remove _base_out.png Merged/_untitled.$j.png
 
